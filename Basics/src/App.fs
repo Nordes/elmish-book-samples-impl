dc52a06695508bb2819d02b79832efbce27c0c75
Nordes Menard-Lamarre <nordes.menard-lamarre@konicaminolta.com>
2020-03-25 13:47:59 +0900

    02 completed https://zaid-ajaj.github.io/the-elmish-book/#/chapters/fable/counter

--- Basics/src/App.fs
+++ Basics/src/App.fs
@@ -2,11 +2,44 @@ module App
 
 open Browser.Dom // Access to JS dom objects. [Fable binding]
 
-// https://zaid-ajaj.github.io/the-elmish-book/#/chapters/fable/hello-world
-printfn "Fable is up and running..."
+let incrementButton = document.getElementById "increment"
+let decrementButton = document.getElementById "decrement"
+let increaseDelayedBtn = document.getElementById "increaseDelayed"
+let countViewer = document.getElementById "countViewer"
 
-let printMsgButton = document.getElementById "printMsg"
+let mutable currentCount = 0 // Bad practice : Mutable
+let rnd = System.Random()
 
-printMsgButton.onclick <- fun eventArgs ->
-    printfn "Button clicked"
+let setCountText currentCount =
+    // set the count viewer with the initial count
+    countViewer.innerText <- sprintf "Count is at %d" currentCount
 
+// attach event handlers
+incrementButton.onclick <- fun ev ->
+    // update the state
+    currentCount <- currentCount + rnd.Next(5, 10)
+    // update the view
+    setCountText currentCount
+
+decrementButton.onclick <- fun ev ->
+    // update the state
+    currentCount <- currentCount -  rnd.Next(5, 10)
+    // update the view
+    setCountText currentCount
+
+// Runs the callback after a delay
+let runAfter ms callback =
+  async {
+    do! Async.Sleep ms
+    do callback()
+  }
+  |> Async.StartImmediate
+
+increaseDelayedBtn.onclick <- fun _ ->
+  runAfter 1000 (fun () ->
+    currentCount  <- currentCount  + rnd.Next(5, 10)
+    setCountText currentCount
+  )
+
+// set the count viewer with the initial count
+setCountText currentCount
