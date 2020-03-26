b7228870bdd2bb6f7f6d4b998e75d4cdcdb405bd
Nordes Menard-Lamarre <nordes.menard-lamarre@konicaminolta.com>
2020-03-26 14:25:33 +0900

    Complete the section of conditional rendering

--- ElmTEA/src/App.fs
+++ ElmTEA/src/App.fs
@@ -23,18 +23,40 @@ let update (msg: Msg) (state: State): State =
         { state with Count = state.Count - 1 }
 
 let render (state: State) (dispatch: Msg -> unit) =
+  let headerText =
+    if state.Count % 2 = 0
+    then "Count is even"
+    else "Count is odd"
+
+  let oddOrEvenMessage =  if state.Count > 0 then Html.h1 headerText else Html.none
   Html.div [
-    Html.button [
-      prop.onClick (fun _ -> dispatch Increment)
-      prop.text "Increment"
+    prop.style [
+      style.textAlign.center
     ]
+    prop.children [
+      Html.h1 [
+          prop.style [ 
+            // (if state.Count < 0 then style.display.none else style.display.block)
+            state.Count < 0, [ style.display.none ] // Same as previous line
+          ] 
+          prop.text headerText
+      ]      
 
-    Html.button [
-      prop.onClick (fun _ -> dispatch Decrement)
-      prop.text "Decrement"
+      Html.button [
+        prop.onClick (fun _ -> dispatch Increment)
+        prop.text "Increment"
+      ]
+      
+      Html.h1 state.Count
+
+      Html.button [
+        prop.onClick (fun _ -> dispatch Decrement)
+        prop.text "Decrement"
+      ]
+
+      oddOrEvenMessage
+      // if state.Count > 0 then yield oddOrEvenMessage // <=== if all is being yield.
     ]
-
-    Html.h1 state.Count
   ]
 
 Program.mkSimple init update render
