0f4beaa57f813d613c9deebfd926bb6159d6bd9a
Nordes Lamarre <nordes@gmail.com>
2020-03-27 14:20:08 +0900

    Todo-part1 completed

--- TodoListApp/src/App.fs
+++ TodoListApp/src/App.fs
@@ -4,54 +4,103 @@ open Elmish
 open Elmish.React
 open Feliz
 
-type State = { 
-  TextInput : string 
-  NumberInput : int
-  }
+type State = {
+  TodoList : string list
+  NewTodo : string
+}
 
 type Msg =
-    | SetTextInput of string
-    | SetNumberInput of int
+  | SetNewTodo of string
+  | AddTodo
 
-let init() =
-    { TextInput = ""; NumberInput = 0 }
+let init() : State =
+    { TodoList = []; NewTodo = "" }
 
-let update (msg: Msg) (state: State): State =
+let update msg state =
     match msg with
-    | SetTextInput newVal ->
-        { state with TextInput = newVal }
-    | SetNumberInput number ->
-        { state with NumberInput = number } // Missing validation
+    | SetNewTodo todoText -> { state with NewTodo = todoText }
+    | AddTodo when state.NewTodo = "" -> state
+    | AddTodo ->
+        { state with
+            NewTodo = ""
+            TodoList = List.append state.TodoList [state.NewTodo] }
 
-let render (state: State) (dispatch: Msg -> unit) =
-  let renderFooter =
-    Html.footer [
-      prop.style [
-        style.fontSize 12
-        style.textAlign.center
-        style.paddingTop 15
+let todoInputField (state: State) (dispatch: Msg -> unit) =
+  Html.div [
+    prop.classes [ "field"; "has-addons" ]
+    prop.children [
+      Html.div [
+        prop.classes [ "control"; "is-expanded"]
+        prop.children [
+          Html.input [
+            prop.classes [ "input"; "is-medium" ]
+            prop.valueOrDefault state.NewTodo
+            prop.onTextChange (SetNewTodo >> dispatch)
+          ]
+        ]
       ]
-      prop.children [
-        Html.hr []
-        Html.text "Ref.: "
-        Html.a [
-          prop.text "The elmish book"
-          prop.href "https://zaid-ajaj.github.io/the-elmish-book/#/chapters/elm/todo-app"
+
+      Html.div [
+        prop.className "control"
+        prop.children [
+          Html.button [
+            prop.classes [ "button"; "is-primary"; "is-medium" ]
+            prop.onClick (fun _ -> dispatch AddTodo)
+            prop.children [
+              Html.i [ prop.classes [ "fa"; "fa-plus" ] ]
+            ]
+          ]
         ]
       ]
     ]
+  ]
 
-  Html.div [
-    Html.h1 [
-      prop.style [
-        style.textAlign.center
+let todoList (state: State) (dispatch: Msg -> unit) =
+  Html.ul [
+    prop.children [
+      for todo in state.TodoList ->
+        Html.li [
+          prop.classes ["box"; "subtitle"]
+          prop.text todo
+        ]
+    ]
+  ]
+
+let appFooter =
+  Html.footer [
+    prop.style [
+      style.fontSize 12
+      style.textAlign.center
+      style.paddingTop 15
+    ]
+    prop.children [
+      Html.hr []
+      Html.text "Ref.: "
+      Html.a [
+        prop.text "The elmish book"
+        prop.href "https://zaid-ajaj.github.io/the-elmish-book/#/chapters/elm/todo-app-part1"
       ]
+    ]
+  ]
+let appTitle =
+  Html.p [
+    prop.className "title"
+    prop.text "Elmish To-Do List"
+  ]
+
+let render (state: State) (dispatch: Msg -> unit) =
+  Html.div [
+    appTitle
+
+    Html.div [
+      prop.style [ style.margin 50 ]
       prop.children [
-        Html.text "Todo List Part 1"
+        todoInputField state dispatch
+        todoList state dispatch
       ]
     ]
 
-    renderFooter
+    appFooter
   ]
 
 Program.mkSimple init update render
