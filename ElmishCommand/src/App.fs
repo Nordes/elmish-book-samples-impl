e3453728431d04309787a4729564a25ad7c093d4
Nordes Lamarre <nordes@gmail.com>
2020-03-30 13:55:35 +0900

    Async update

--- ElmishCommand/src/App.fs
+++ ElmishCommand/src/App.fs
@@ -5,28 +5,48 @@ open Elmish.React
 open Feliz
 
 type State =
-    { Count: int }
+    { 
+      Count: int 
+      Loading : bool}
 
 type Msg =
     | Increment
     | Decrement
+    | IncrementDelayed
 
 let init() =
-    { Count = 0 }, Cmd.none
+    { Count = 0 ; Loading = false }, Cmd.none
 
 let update msg state =
     match msg with
     | Increment ->
-        { state with Count = state.Count + 1 }, Cmd.none
+        { state with Loading = false; Count = state.Count + 1 }, Cmd.none
 
     | Decrement ->
         { state with Count = state.Count - 1 }, Cmd.none
+
+    | IncrementDelayed when state.Loading -> state, Cmd.none
+    | IncrementDelayed ->
+      let incrementDelayedCmd (dispatch: Msg -> unit) : unit =
+          let delayedDispatch = async {
+              do! Async.Sleep 1000
+              dispatch Increment
+          }
+
+          Async.StartImmediate delayedDispatch
+
+      { state with Loading = true }, Cmd.ofSub incrementDelayedCmd
 
 let render (state: State) (dispatch: Msg -> unit) =
   let headerText =
     if state.Count % 2 = 0
     then "Count is even"
     else "Count is odd"
+
+  let content =
+    if state.Loading
+    then Html.h1 "LOADING..."
+    else Html.h1 state.Count
 
   let oddOrEvenMessage =  if state.Count > 0 then Html.h1 headerText else Html.none
   Html.div [
@@ -46,8 +66,13 @@ let render (state: State) (dispatch: Msg -> unit) =
         prop.onClick (fun _ -> dispatch Increment)
         prop.text "Increment"
       ]
+      Html.button [
+        prop.disabled state.Loading // <-- Disable the button if we click on it (state loading)
+        prop.onClick (fun _ -> dispatch IncrementDelayed)
+        prop.text "Increment Delayed"
+      ]
       
-      Html.h1 state.Count
+      content
 
       Html.button [
         prop.onClick (fun _ -> dispatch Decrement)
@@ -56,6 +81,9 @@ let render (state: State) (dispatch: Msg -> unit) =
 
       oddOrEvenMessage
       // if state.Count > 0 then yield oddOrEvenMessage // <=== if all is being yield.
+      Html.p [
+        prop.text "https://zaid-ajaj.github.io/the-elmish-book/#/chapters/commands/async-updates"
+      ]
     ]
   ]
 
